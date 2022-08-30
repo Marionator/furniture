37f141d979b5969e6aa3a9453df5bfbf7342f3ff
Yunfei Zhao <y.zhao101@lse.ac.uk>
2022-08-30 12:05:40 +0100

    itemscontroller

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,13 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_many :bookings
+  validates :name, presence: true
+  validates :type, presence: true
+  validates :status, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+  validates :width, presence: true
+  validates :height, presence: true
+  validates :depth, presence: true
+  validates :shipping, presence: true
 end
