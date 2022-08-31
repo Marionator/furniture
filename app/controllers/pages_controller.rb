2e28bfac815adfab17a87d2e7321d465a7bcad92
Zhanna Doronina <doronina.jane@mail.ru>
2022-08-31 13:55:27 +0100

    commiting to pull master

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
-    @most_recent_items = Item.last(4)
+    @most_recent_items = Item.last(2)
   end
 end
